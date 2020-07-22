1d61c5eb3fe2f1ed4e2e3b253d92b62b48c41ff5
Sejadis <sejadis-dev@gmx.de>
2020-07-22 17:03:56 +0200

    add statemanagers for upgrades / abilities
    Signed-off-by: Sejadis <sejadis-dev@gmx.de>

---
+++ Assets/Scripts/Systems/Abilities/AbilityManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SejDev.Save;
+using SejDev.Systems.Save;
+using UnityEngine;
+
+
+namespace SejDev.Systems.Abilities
+{
+    public static class AbilityManager
+    {
+        public static Dictionary<string, bool> unlockStates;
+
+        public static void LoadAbilityStates()
+        {
+            // AbilityStateSave save = SaveSystem.Load<AbilityStateSave>("states", SaveType.sav);
+            var save = SaveManager.GetSave<AbilityStateSave>();
+            var dict = save?.states.ToDictionary(s => s.id, s => s.isUnlocked);
+            unlockStates = dict ?? new Dictionary<string, bool>();
+        }
+
+        public static void SaveAbilityStates()
+        {
+            AbilityStateSave save = new AbilityStateSave(unlockStates);
+            SaveManager.SetSave(save);
+            // SaveSystem.Save(save, "states", SaveType.sav);
+        }
+    }
+}
---
+++ Assets/Scripts/Systems/Abilities/UpgradeManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SejDev.Save;
+using SejDev.Systems.Save;
+
+namespace SejDev.Systems.Abilities
+{
+    public static class UpgradeManager
+    {
+        static Dictionary<string, UpgradeState> states;
+        private static bool isLoaded;
+
+        public static List<string> GetActiveUpgrades(List<string> upgradeIDs)
+        {
+            foreach (var id in upgradeIDs)
+            {
+                GetActiveState(id);
+            }
+
+            return states.Where(
+                    kvp => upgradeIDs.Contains(kvp.Key) &&
+                           kvp.Value.isActive //check ids from parameter for active state
+                )
+                .Select(kvp => kvp.Key) // take only key from resulting KVP enumerator
+                .ToList();
+        }
+
+        public static void Load()
+        {
+            // UpgradeStateSave save = SaveSystem.Load<UpgradeStateSave>("upgradeStates", SaveType.sav);
+            var save = SaveManager.GetSave<UpgradeStateSave>();
+            var dict = save?.states.ToDictionary(s => s.id, s => s.state);
+            states = dict ?? new Dictionary<string, UpgradeState>();
+            isLoaded = true;
+        }
+
+        public static void Save()
+        {
+            UpgradeStateSave save = new UpgradeStateSave(states);
+            SaveManager.SetSave(save);
+            // SaveSystem.Save(save, "upgradeStates", SaveType.sav);
+        }
+
+        public static bool GetActiveState(string id)
+        {
+            if (!isLoaded)
+            {
+                Load();
+            }
+
+            if (!states.ContainsKey(id))
+            {
+                states[id] = new UpgradeState();
+            }
+
+            return states[id].isActive;
+        }
+
+        public static void SetActive(string upgradeGuid, bool isActive)
+        {
+            if (!isLoaded)
+            {
+                Load();
+            }
+
+            states[upgradeGuid].isActive = isActive;
+            Save();
+        }
+    }
+}

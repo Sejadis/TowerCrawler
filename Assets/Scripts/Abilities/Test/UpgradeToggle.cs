fac3fa9235d34ab2d000fb4c693dc47907e08af2
Sejadis <sejadis-dev@gmx.de>
2020-07-22 17:08:03 +0200

    add test script + trigger for upgrade activation
    Signed-off-by: Sejadis <sejadis-dev@gmx.de>

---
+++ Assets/Scripts/Abilities/Test/UpgradeToggle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SejDev.Systems.Abilities;
+using UnityEngine;
+
+namespace SejDev.Abilities.Test
+{
+    public class UpgradeToggle : MonoBehaviour
+    {
+        public bool isActive;
+        public AbilityUpgrade upgrade;
+
+        private void Start()
+        {
+            isActive = UpgradeManager.GetActiveState(upgrade.GUID);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            isActive = !isActive;
+            UpgradeManager.SetActive(upgrade.GUID, isActive);
+        }
+    }
+}

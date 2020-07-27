0c2116ed1750f1f80323d3b024f8eeed8f7bce43
Sejadis <sejadis-dev@gmx.de>
2020-07-27 21:56:26 +0200

    add NoEdit editor attribute
    Signed-off-by: Sejadis <sejadis-dev@gmx.de>

---
+++ Assets/Scripts/Editor/NoEditEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace SejDev.Editor
+{
+    [CustomPropertyDrawer(typeof(NoEditAttribute))]
+    public class NoEditEditor : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property,
+            GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
+    }
+}

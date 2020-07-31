badc64fde44e4fb501956fb39cc5e37bb2ac310d
Sejadis <sejadis-dev@gmx.de>
2020-07-31 15:09:39 +0200

    add playercontroller component for large player scope operations add gamemanager for game scope operations
    Signed-off-by: Sejadis <sejadis-dev@gmx.de>

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using SejDev.Systems.Abilities;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public PlayerController player;
+
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("More than 1 GameManager in the scene, destroying this", this);
+            Destroy(this);
+        }
+
+        Instance = this;
+    }
+
+    public void DisablePlayer()
+    {
+        InputManager.Instance.PlayerInput.Abilities.Disable();
+        InputManager.Instance.PlayerInput.Controls.Disable();
+        player.DisablePlayer();
+    }
+
+    public void EnablePlayer()
+    {
+        InputManager.Instance.PlayerInput.Abilities.Enable();
+        InputManager.Instance.PlayerInput.Controls.Enable();
+        player.GetComponent<AbilityHandler>()?.ReloadAbilities();
+        player.EnablePlayer();
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using SejDev.Player;
+using SejDev.Systems.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private MouseLook mouseLook;
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private UIScreen playerGameScreen;
+
+    public void DisablePlayer()
+    {
+        mouseLook.enabled = false;
+        playerMovement.enabled = false;
+        playerGameScreen.Hide();
+    }
+
+    public void EnablePlayer()
+    {
+        mouseLook.enabled = true;
+        playerMovement.enabled = true;
+        playerGameScreen.Show();
+    }
+}

045c61e48a47781262cc196efbeae7ba97e59ce3
Sejadis <sejadis-dev@gmx.de>
2021-02-19 14:24:14 +0100

    added sword ability
    Signed-off-by: Sejadis <sejadis-dev@gmx.de>

---
+++ Assets/Scripts/Abilities/SwordBaseAbility.cs
@@ -0,0 +1,21 @@
+﻿using SejDev.Editor;
+using SejDev.Systems.Abilities;
+using SejDev.Systems.Core;
+using UnityEngine;
+
+namespace SejDev.Abilities
+{
+    [CreateAssetMenu(fileName = "Assets/Resources/Abilities/NewSwordBaseAbility",
+        menuName = "Systems/Ability/Sword Base Ability")]
+    public partial class SwordBaseAbility : WeaponAbility
+    {
+        [field: SerializeField, Rename] public int Damage { get; private set; }
+
+        protected override void TriggerHitEffect(object target)
+        {
+            Transform hit = target as Transform;
+            IDamagable damagable = hit.GetComponent<IDamagable>();
+            damagable?.TakeDamage(this, Damage);
+        }
+    }
+}

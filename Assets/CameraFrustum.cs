56723c4dc52e4397c5ae008bf4663fa4ea4134b9
Sejadis <sejadis-dev@gmx.de>
2021-02-19 14:26:07 +0100

    add basic inventory ui to scene
    Signed-off-by: Sejadis <sejadis-dev@gmx.de>

---
+++ Assets/CameraFrustum.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraFrustum : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+
+    private void OnDrawGizmos()
+    {
+        if (cam == null) return;
+
+        Gizmos.DrawFrustum(transform.position, cam.fieldOfView, cam.farClipPlane, cam.nearClipPlane, cam.aspect);
+    }
+}
